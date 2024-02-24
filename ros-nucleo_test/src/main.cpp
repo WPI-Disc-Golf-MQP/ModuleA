33d7e54e062a5c4e35e350debb2ad66d9f501862
Benjamin Antupit <b@antupit.com>
2024-02-24 14:50:42 -0500

    adapt to new std node

--- ros-nucleo_test/src/main.cpp
+++ ros-nucleo_test/src/main.cpp
@@ -8,23 +8,19 @@
 
 
 // ----- INTAKE -----  
+MODULE* intake_module;
+
 int BEAM_BREAK_PIN = D2;
 int INTAKE_SPEED_PIN = A0;
 int INTAKE_INVERT_PIN = D13;
 int UPPER_SPEED_PIN = A1;
 int UPPER_INVERT_PIN = D6;
 
-// intake feedback
-std_msgs::Bool intake_state_msg;
-String _intake_state_topic("intake_state");
-ros::Publisher intake_state_pub(_intake_state_topic.c_str(), &intake_state_msg);
-
 enum INTAKE_STATE {
   INTAKE_IDLE = 0,
   INTAKE_SEND = 1, // sending a disc out of the intake onto the conveyor 
   INTAKE_RECIEVE = 2, // getting a disc from the top conveyor into the intake 
   };
-
 INTAKE_STATE intake_state = INTAKE_STATE::INTAKE_IDLE;
 
 bool is_disc_present = false;
@@ -49,7 +45,7 @@ void top_motor_stop() {
   analogWrite(UPPER_SPEED_PIN, 0); // stop
 }
 
-void stop() {
+void stop_intake() {
   top_motor_stop();
   intake_motor_stop();
 }
@@ -58,7 +54,7 @@ bool beam_broken() {
   return (digitalRead(BEAM_BREAK_PIN) == 0);
 }
 
-bool verify_motion_complete() {
+bool verify_intake_complete() {
   return intake_state == INTAKE_STATE::INTAKE_IDLE;
 }
 
@@ -98,6 +94,7 @@ void check_intake() {
       if (!beam_broken()){ // therefore disc has come in, and gone past the first green wheel
         is_disc_present = true;
         intake_state = INTAKE_STATE::INTAKE_IDLE;
+        intake_module->publish_status(MODULE_STATUS::COMPLETE);
 
         // TODO: determine if it is ok for it to go from idle -> recieve -> idle. This code may not be necessary 
         // this if statement ensures that a disc has been deposited before going back to the idle state. Basically ensuring that it doesn't go from idle -> recieve -> idle 
@@ -113,27 +110,17 @@ void check_intake() {
       logwarn("intake state invalid");
       break;
   }
-
-
-  // publish state to pi 
-  if (intake_state_msg.data != (int) intake_state) {
-      intake_state_msg.data = intake_state;
-      loginfo("publishing intake state, " + String(intake_state));
-      intake_state_pub.publish(&intake_state_msg);
-  }
+  intake_module->publish_state((int) intake_state);
 }
 
 // ----- CAMERA TURNTABLE ----- 
 
+MODULE* turntable_module;
 int dir_pin = D11;
 int step_pin = D12;
 int sleep_pin = D6; 
 int UPPER_LIMIT_SWITCH_PIN = D3; // FINISH THIS FUNCTION 
 int LOWER_LIMIT_SWITCH_PIN = D4; // FINISH THIS FUNCTION 
-
-std_msgs::Bool turntable_state_msg;
-String _turntable_state_topic(NODE_NAME + "_feedback__intake_state");
-ros::Publisher turntable_state_pub(_turntable_state_topic.c_str(), &turntable_state_msg);
 
 enum TURNTABLE_STATE {
   TURNTABLE_IDLE = 0,
@@ -152,6 +139,10 @@ bool lower_limit_switched() { // FINISH THIS FUNCTION after wiring
   return (digitalRead(LOWER_LIMIT_SWITCH_PIN) == 1);
 }
 
+bool verify_turntable_complete() {
+  return turntable_state == TURNTABLE_STATE::TURNTABLE_IDLE;
+}
+
 bool run_yaxis_motor = false;
 long yaxis_motor_last_step = millis();
 bool yaxis_motor_last_digital_write = false;
@@ -167,15 +158,18 @@ void start_turntable() {
   yaxis_motor_last_step = millis();
 }
 
+void stop_turntable() {
+  run_yaxis_motor = false; 
+  run_spin_motor = false; 
+  turntable_state = TURNTABLE_STATE::TURNTABLE_IDLE;
+}
+
 
 void check_turntable() {
 
-  Serial.println(yaxis_motor_last_step);
-
   // drive the motor if the flag has been set to run it // TODO implimet the sleep pin as well 
   if ((yaxis_motor_last_step+2 < millis()) && run_yaxis_motor == true) {
-    Serial.println("triggered correctly");
-    Serial.println(!yaxis_motor_last_digital_write);
+    loginfo("triggered correctly");
 
     // digitalWrite(step_pin, !yaxis_motor_last_digital_write);
     digitalWrite(step_pin, HIGH);
@@ -216,17 +210,12 @@ void check_turntable() {
         run_yaxis_motor = false; 
         run_spin_motor = false; 
         turntable_state = TURNTABLE_STATE::TURNTABLE_IDLE; 
+        turntable_module->publish_status(MODULE_STATUS::COMPLETE);
       }
       
       break;
   }
-
-  // publish state to pi 
-  if (turntable_state_msg.data != (int) turntable_state) {
-      turntable_state_msg.data = turntable_state;
-      loginfo("publishing turntable state");
-      turntable_state_pub.publish(&turntable_state_msg);
-  }
+  turntable_module->publish_state((int) turntable_state);
 };
 
 
@@ -240,13 +229,17 @@ void check_turntable() {
 
 void setup() {
   init_std_node();
-  init_module("intake",
+  loginfo("setup() Start");
+  intake_module = init_module("intake",
     start_intake,
-    verify_motion_complete,
-    stop, 
+    verify_intake_complete,
+    stop_intake, 
     [] () {}/* TODO: add calibration routine if needed */);
-
-  nh.advertise(intake_state_pub);
+  turntable_module = init_module("turntable",
+    start_turntable,
+    verify_turntable_complete,
+    stop_turntable, 
+    [] () {}/* TODO: add calibration routine if needed */);
 
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -266,8 +259,6 @@ void setup() {
 
 
   loginfo("setup() Complete");
-
-  Serial.begin(57600);
 }
 
 // ---- testing ---- 
@@ -294,8 +285,7 @@ void setup() {
 void loop() {
   periodic_status();
   nh.spinOnce();
-  delay(10);
   check_intake();
-  // check_turntable();
+  check_turntable();
 }
 
